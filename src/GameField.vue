889f58132272c9bcdef2d407af91141ef5f8e075
DogFox <kurovda@gmail.com>
2021-12-15 15:36:43 +0300

    add base circles

--- src/GameField.vue
+++ src/GameField.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="m-auto w-6/12">
-    <div class="game-field">
+    <div
+      id="grid"
+      class="game-field"
+    >
       <!-- <div class="grid grid-cols-9"> -->
       <template
         v-for="i in 81"
         :key="i"
       >
         <div class="border border-black cell">
-          {{ i }}
+          <div class="circle">
+            {{ i }}
+          </div>
         </div>
       </template>
     <!-- </div> -->
@@ -16,7 +21,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 
 export default defineComponent({
   name: 'App',
@@ -25,8 +30,18 @@ export default defineComponent({
   setup() {
     const arr = 'test';
 
+    const iterator = function() {
+      const grid = document.getElementById('grid');
+      console.log(grid);
+    };
+
+    onMounted(() => {
+      iterator();
+    });
+
     return {
       arr,
+
     };
   },
 });
@@ -35,9 +50,20 @@ export default defineComponent({
 <style lang="scss">
 .game-field {
   display: grid;
-  grid-template-columns: repeat(9, 1fr);;
+  grid-template-columns: repeat(9, 1fr);
+  grid-gap: 0px 0px;
 }
 .cell {
   height: 60px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+}
+.circle {
+   background-color: red;
+   border-radius: 50%;
+   border: 1px solid #0F1C3F;
+   height: 55px;
+   width: 55px;
 }
 </style>
