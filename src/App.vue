76ec82d1f679d6975276a9890669d336da75fb9d
DogFox <kurovda@gmail.com>
2021-12-15 09:23:14 +0300

    game field

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,21 @@
 <template>
-  <div class="border-solid border-2">
-    fsfs
-
-    <h1 class="text-3xl font-bold underline decoration-wavy">
-      Hello world!
-    </h1>
-  </div>
-  <div class="m-auto w-6/12">
-    <div class="game-field">
-      <!-- <div class="grid grid-cols-9"> -->
-      <template
-        v-for="i in 81"
-        :key="i"
-      >
-        <div class="border border-black cell">
-          {{ i }}
-        </div>
-      </template>
-    <!-- </div> -->
-    </div>
-  </div>
+  <h1 class="text-3xl font-bold ">
+    Color Lines!
+  </h1>
+  <game-field class="py-10" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import GameField from './GameField.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+    GameField,
   },
   setup() {
-    const arr = 'test';
-
     return {
-      arr,
     };
   },
 });
@@ -47,12 +29,5 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.game-field {
-  display: grid;
-  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px 50px;
-}
-.cell {
-  height: 50px;
 }
 </style>
---
+++ src/GameField.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="m-auto w-6/12">
+    <div class="game-field">
+      <!-- <div class="grid grid-cols-9"> -->
+      <template
+        v-for="i in 81"
+        :key="i"
+      >
+        <div class="border border-black cell">
+          {{ i }}
+        </div>
+      </template>
+    <!-- </div> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+  setup() {
+    const arr = 'test';
+
+    return {
+      arr,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.game-field {
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);;
+}
+.cell {
+  height: 60px;
+}
+</style>
