e170b17c676f97506b7f2fd342fe702f71b6575f
DogFox <kurovda@gmail.com>
2021-12-15 08:55:30 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="border-solid border-2">
+    fsfs
+
+    <h1 class="text-3xl font-bold underline decoration-wavy">
+      Hello world!
+    </h1>
+  </div>
+  <div class="m-auto w-6/12">
+    <div class="game-field">
+      <!-- <div class="grid grid-cols-9"> -->
+      <template
+        v-for="i in 81"
+        :key="i"
+      >
+        <div class="border border-black cell">
+          {{ i }}
+        </div>
+      </template>
+    <!-- </div> -->
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
-})
+  },
+  setup() {
+    const arr = 'test';
+
+    return {
+      arr,
+    };
+  },
+});
 </script>
 
 <style lang="scss">
@@ -23,5 +47,12 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.game-field {
+  display: grid;
+  grid-template-columns: 50px 50px 50px 50px 50px 50px 50px 50px 50px;
+}
+.cell {
+  height: 50px;
 }
 </style>
